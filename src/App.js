e10c00d25d87229d89aaa329e36df4e4aab12feb
Patrick Smith <patrick@burntcaramel.com>
2017-11-30 10:36:49 +1100

    Move quote data to state

--- src/App.js
+++ src/App.js
@@ -3,18 +3,33 @@ import './App.css';
 import StockInfo from './components/StockInfo'
 
 class App extends Component {
+  state = {
+    quote: {
+      symbol: 'NFLX',
+      companyName: 'Netflix Inc.',
+      primaryExchange: 'Nasdaq Global Select',
+      latestPrice: 188.15,
+      latestSource: 'Close',
+      week52High: 204.38,
+      week52Low: 113.95
+    }
+  }
+
   render() {
+    // const quote = this.state.quote
+    const { quote } = this.state
+
     return (
       <div className="App">
         <h1>Wolf of React</h1>
         <StockInfo
-          symbol='NFLX'
-          companyName='Netflix Inc.'
-          primaryExchange='Nasdaq Global Select'
-          latestPrice={ 188.15 }
-          latestSource='Close'
-          week52High={ 204.38 }
-          week52Low={ 113.95 }
+          symbol={ quote.symbol }
+          companyName={ quote.companyName }
+          primaryExchange={ quote.primaryExchange }
+          latestPrice={ quote.latestPrice }
+          latestSource={ quote.latestSource }
+          week52High={ quote.week52High }
+          week52Low={ quote.week52Low }
         />
       </div>
     );
