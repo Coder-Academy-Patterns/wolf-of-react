a9df3b6541fa84cf2c15c3ae3c10f2fae1ef9d4d
Patrick Smith <patrick@burntcaramel.com>
2017-11-30 11:07:23 +1100

    Load quote when starting app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 import StockInfo from './components/StockInfo'
+import { loadQuoteForStock } from './api/iex'
 
 class App extends Component {
   state = {
-    quote: {
-      symbol: 'NFLX',
-      companyName: 'Netflix Inc.',
-      primaryExchange: 'Nasdaq Global Select',
-      latestPrice: 188.15,
-      latestSource: 'Close',
-      week52High: 204.38,
-      week52Low: 113.95
-    }
+    quote: null
+  }
+
+  // The first time our component is rendered
+  // this method is called
+  componentDidMount() {
+    loadQuoteForStock('nflx')
+      .then((quote) => {
+        this.setState({ quote: quote })
+      })
   }
 
   render() {
-    // const quote = this.state.quote
     const { quote } = this.state
 
     return (
       <div className="App">
         <h1>Wolf of React</h1>
-        <StockInfo
-          symbol={ quote.symbol }
-          companyName={ quote.companyName }
-          primaryExchange={ quote.primaryExchange }
-          latestPrice={ quote.latestPrice }
-          latestSource={ quote.latestSource }
-          week52High={ quote.week52High }
-          week52Low={ quote.week52Low }
-        />
+        {
+          !!quote ? (
+            <StockInfo
+              symbol={ quote.symbol }
+              companyName={ quote.companyName }
+              primaryExchange={ quote.primaryExchange }
+              latestPrice={ quote.latestPrice }
+              latestSource={ quote.latestSource }
+              week52High={ quote.week52High }
+              week52Low={ quote.week52Low }
+            />
+          ) : (
+            <p>Loading…</p>
+          )
+        }
       </div>
     );
   }
